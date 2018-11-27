d8a1c3e8f961a4a485615ebe7fd92e4025032f31
kgolec93 <kamil.golec@gmail.com>
2018-11-27 20:28:54 +0100

    major changes in layout and scripts

--- script/script.js
+++ script/script.js
@@ -1,6 +1,30 @@
 $(function() {
 
-    //Slider image list
+    //MENU NAV LIST
+    let j = 0;
+    
+    let menuList = [
+        {name: "STRONA GŁÓWNA", link: "index.html"},
+        {name: "PORTFOLIO", link: "portfolio.html"},
+        {name: "KONTAKT", link: "kontakt.html"}
+    ];
+
+    function menuAppend() {
+        for (j=0; j<menuList.length; j++) {
+            menuList[j].link;
+            if(window.location.href.indexOf(menuList[j].link) != -1) {
+                $("#main-menu").append("<li class=\"nav-item\"><a class=\"nav-link active\" href=\" "+ menuList[j].link + "\">" + menuList[j].name + "</a></li>");
+            }
+            else {
+                $("#main-menu").append("<li class=\"nav-item\"><a class=\"nav-link\" href=\" "+ menuList[j].link + "\">" + menuList[j].name + "</a></li>");
+            }            
+        }
+    };
+
+    menuAppend();
+    
+    
+    //SLIDER BANNER IMAGES
     let sliderList = [
     "slider_01.jpg", 
     "slider_02.jpg", 
@@ -45,7 +69,17 @@ $(function() {
     //WEBSITE CONTENT
     //INDEX CONTENT
     const indexContent = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam quis lacus eu leo consequat lacinia eu eget tellus. Curabitur eget lacus felis. Nullam bibendum, justo sed venenatis varius, orci lacus interdum dui, ut aliquam felis risus gravida odio. In aliquam vestibulum massa, in pulvinar metus blandit sed. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Sed molestie quam vitae condimentum rhoncus. Mauris odio enim, interdum non quam a, porttitor porttitor magna. Nullam maximus, quam at imperdiet ullamcorper, metus nisl elementum turpis, eu pulvinar nulla nisi eget elit. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Nulla facilisis enim dui, sit amet malesuada nunc tempor eu. Pellentesque volutpat luctus metus id egestas. Nullam lobortis, arcu quis eleifend pulvinar, dolor justo convallis arcu, condimentum tempus arcu eros vitae augue. Phasellus id suscipit mauris. Mauris cursus ipsum vel lorem rhoncus eleifend. ";
-    $("#text-container").append("<p>"+indexContent+"</p>");
+    $("#index-container").append("<p>"+indexContent+"</p>");
 
+    //CONTACT CONTENT
+    const contactContent = "<h1><span style=\"font-family:Arkitech\">digital.arch</span></h1><br>Mgr inż. arch. Kamil Golec<br>tel: +48787787787<br>email: info@digitalarch.pl<br><br>";
+    $("#contact-container").append("<p>"+contactContent+"</p>");
+
+
+    //ABOUT US CONTENT
+    const aboutContent = "Jesteśmy nowoczesnym studiem graficznym z Wrocławia, zajmującym się wszelkiego rodzaju wizualizacjami 3d. Pod szyldem digital.ARCH działamy od 2018 roku, lecz nasze korzenie sięgają znacznie dalej. Staramy się stale rozwijać system naszej pracy o najnowsze technologie, abyśmy mogli dostarczyć Państwu wizualizacje w jeszcze lepszej jakości. W naszych grafikach staramy się dążyć do osiągnięcia fotorealizmu, jednak nie boimy się również bardziej wymagających odrobiny artyzmu. Zapraszamy do zapoznania się i skorzystania z naszej oferty, nie tylko skierowanej do architektów oraz deweloperów!";
+    $("#contact-container").append("<p>"+aboutContent+"</p>");
+
+    
 
 });
