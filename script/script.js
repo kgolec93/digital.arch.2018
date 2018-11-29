76fd1bf599ff5bd14322947f95459aaa03425f67
kamil_golec <kamil.golec@gmail.com>
2018-11-29 23:23:44 +0100

    major changes

--- script/script.js
+++ script/script.js
@@ -24,7 +24,6 @@ $(function() {
 
     menuAppend();
     
-    
     //SLIDER BANNER IMAGES
     let sliderList = [
     "slider_01.jpg", 
@@ -69,7 +68,7 @@ $(function() {
 
     //WEBSITE CONTENT
     //INDEX CONTENT
-    const indexContent = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam quis lacus eu leo consequat lacinia eu eget tellus. Curabitur eget lacus felis. Nullam bibendum, justo sed venenatis varius, orci lacus interdum dui, ut aliquam felis risus gravida odio. In aliquam vestibulum massa, in pulvinar metus blandit sed. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Sed molestie quam vitae condimentum rhoncus. Mauris odio enim, interdum non quam a, porttitor porttitor magna. Nullam maximus, quam at imperdiet ullamcorper, metus nisl elementum turpis, eu pulvinar nulla nisi eget elit. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Nulla facilisis enim dui, sit amet malesuada nunc tempor eu. Pellentesque volutpat luctus metus id egestas. Nullam lobortis, arcu quis eleifend pulvinar, dolor justo convallis arcu, condimentum tempus arcu eros vitae augue. Phasellus id suscipit mauris. Mauris cursus ipsum vel lorem rhoncus eleifend. ";
+    const indexContent = "Od 5 lat zajmujemy się realizacją wszelkiego rodzaju wizualizacji trójwymiarowych, przede wszystkim architektury oraz wnętrz, a także wizualizacji produktu oraz animacji. Stale rozwijamy technologię naszej pracy, dzięki czemu stale możemy poprawiać jakość naszych realizacji, a także rozwijać naszą ofertę o kolejne możliwości. Zapraszamy do zapoznania się z portfolio naszych realizacji oraz z naszą bogatą ofertą.";
     $("#index-container").append("<p>"+indexContent+"</p>");
 
     //CONTACT CONTENT
