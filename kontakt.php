6a1037ad943eb8e4bba7f41f73527066dea4e871
kgolec93 <kamil.golec@gmail.com>
2019-01-09 07:24:15 +0100

    Ajax contact form and uploaded to the server

--- kontakt.php
+++ kontakt.php
@@ -1,26 +1,38 @@
 <?php
-$name = $_POST['form-name'];
-$email = $_POST['form-email'];
-$message = $_POST['form-message'];
-$sender = "digitalarch@digitalarch.pl";
-$reciever = "digitalarch@digitalarch.pl";
-$title = "Formularz kontaktowy digital.ARCH";
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+        $name = strip_tags(trim($_POST["name"]));
+				$name = str_replace(array("\r","\n"),array(" "," "),$name);
+        $email = filter_var(trim($_POST["email"]), FILTER_SANITIZE_EMAIL);
+        $message = trim($_POST["message"]);
 
-$messageValue = "";
-$messageValue .= "Imię i nazwisko: " . $name . "\n";
-$messageValue .= "Email nadawcy: " . $email . "\n";
-$messageValue .= "Wiadomość: " . $message . "\n";
+        if ( empty($name) OR empty($message) OR !filter_var($email, FILTER_VALIDATE_EMAIL)) {
+            http_response_code(400);
+            echo "Wystąpił błąd. Wypełnij wszystkie pola jeszcze raz";
+            exit;
+        }
 
-$header = "";
-$header .= "Od: " . $email . "\n";
-$header .= "Content-Type:text/plain;charset=utf-8";
+        $recipient = "digitalarch@digitalarch.pl";
 
-$success = mail($reciever, $title, $messageValue, $header);
+        $subject = "Formularz kontaktowy digital.ARCH";
 
-if ($success){
-    print "<meta http-equiv=\"refresh\" content=\"0;URL=kontakt-sukces.html\">";
-}
-// else{
-//     print "<meta http-equiv=\"refresh\" content=\"0;URL=kontakt-error.html\">";
-// }
+        $email_content = "Nadawca: $name\n";
+        $email_content .= "Email: $email\n\n";
+        $email_content .= "Wiadomość:\n$message\n";
+
+        $header = "";
+        $header .= "Od: " . $email . "\n";
+        $header .= "Content-Type:text/plain;charset=utf-8";
+
+        if (mail($recipient, $subject, $email_content, $header)) {
+            http_response_code(200);
+            echo "Twoja wiadomość została wysłana!";
+        } else {
+            http_response_code(500);
+            echo "Wystąpił błąd i wiadomość nie została wysłana";
+        }
+
+    } else {
+        http_response_code(403);
+        echo "Wystąpił problem z Twoją wiadomością. Spróbuj ponownie";
+    }
 ?>
--- script/contact-form.js
+++ script/contact-form.js
@@ -1,63 +1,32 @@
-function formSubmit(){
-    let formAlert = document.getElementById("form-alert")
-    let formName = document.getElementById("form-name")
-    let formEmail = document.getElementById("form-email")
-    let formMessage = document.getElementById("form-message")
-    //no name
-    if (formName.value === "" || formEmail.value === "" || formMessage.value === ""){
-        formAlert.innerHTML = "Proszę wypełnić wszystkie pola";
-    }        
-    else {
-        function validateEmail(email) {
-            var re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-            return re.test(email);
-        }
-        
-        function validate() {
-            var email = document.getElementById("form-email").value;
-        
-            if (validateEmail(email)) {
-                document.getElementById('contact-form').submit();
-                formAlert.innerHTML = "Wiadomość została wysłana!";
-                formName.value = "";
-                formEmail.value = "";
-                formMessage.value = "";
-            } 
-            //invalid email adress
-            else {
-                formAlert.innerHTML = "Wpisany email jest nieprawidłowy!";
+$(function() {
+    var form = document.getElementById('contact-form');
+    var formMessages = document.getElementById('form-alert');
+
+    $(form).submit(function(event) {
+        event.preventDefault();
+        var formData = $("#contact-form").serialize();
+        $.ajax({
+            type: 'POST',
+            url: $("#contact-form").attr('action'),
+            data: formData
+        })
+        .done(function(response) {
+            $(formMessages).text(response);
+
+            $('#form-name').val('');
+            $('#form-email').val('');
+            $('#form-message').val('');
+        })
+        .fail(function(data) {
+
+            if (data.responseText !== '') {
+                $(formMessages).text(data.responseText);
+            } else {
+                $(formMessages).text('Wystąpił błąd i wiadomość nie została wysłana');
             }
-            return false;
-        }  
-    validate(); 
-    }
-}    
-//Contact submit click listener
-document.getElementById("form-button").addEventListener("click", formSubmit);
-
-// $(function() {
-//     // Get the form.
-//     var form = document.getElementById('contact-form');
-
-//     // Get the messages div.
-//     var formMessages = document.getElementById('form-alert');
-
-//     // TODO: The rest of the code will go here...
-//     $(form).submit(function(event) {
-//         // Stop the browser from submitting the form.
-//         event.preventDefault();
-    
-//         // TODO
-//     });
-//     // Serialize the form data.
-//     var formData = $(form).serialize();
-
-//     // Submit the form using AJAX.
-//     $.ajax({
-//         type: 'POST',
-//         url: $(form).attr('action'),
-//         data: formData
-//     })
-// });
+        });
+    });
+       
+});
 
 
